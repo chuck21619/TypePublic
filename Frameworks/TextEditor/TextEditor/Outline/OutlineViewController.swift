abdc71eba0cdf4198e6f1ee9ba18daa9cbc25aeb
chuck21619 <charlesjohnston@charless-MBP.ernieJohnston.com>
2018-11-01 17:48:39 -0400

    start using NSOutlineView for text group outline view

--- Frameworks/TextEditor/TextEditor/Outline/OutlineViewController.swift
+++ Frameworks/TextEditor/TextEditor/Outline/OutlineViewController.swift
@@ -8,10 +8,14 @@
 
 import Foundation
 
-class OutlineViewController: NSViewController, OutlineModelDelegate {
+class OutlineViewController: NSViewController, OutlineModelDelegate, NSOutlineViewDataSource, NSOutlineViewDelegate {
     
     // MARK: - Properties
     var model: OutlineModel? = nil
+    var parentTextGroup = TextGroup(title: "parent")
+    let columnReuseIdentifier = NSUserInterfaceItemIdentifier(rawValue: "columnReuseIdentifier")
+    
+    @IBOutlet weak var outlineView: NSOutlineView!
     
     // just for testing
     @IBOutlet var testingTextView: NSTextView!
@@ -42,35 +46,108 @@ class OutlineViewController: NSViewController, OutlineModelDelegate {
         
     }
     
+    // MARK: Initialization
+    override func viewDidLoad() {
+        
+        self.outlineView.dataSource = self
+        self.outlineView.delegate = self
+    }
+    
     // MARK: stuff
     private func updateOutline(textGroups: [TextGroup]?) {
         
-        guard let textGroups = textGroups, textGroups.isEmpty == false else {
-            
-            clearOutline()
-            return
-        }
-        
-        print("update outline: \(textGroups)")
+        parentTextGroup.textGroups = textGroups ?? []
         
         DispatchQueue.main.async {
-            self.testingTextView.string = textGroups.description
+            
+            self.outlineView.reloadData()
         }
         
+//        guard let textGroups = textGroups, textGroups.isEmpty == false else {
+//
+//            clearOutline()
+//            return
+//        }
+//
+//        print("update outline: \(textGroups)")
+//
+//        DispatchQueue.main.async {
+//            self.testingTextView.string = textGroups.description
+//        }
     }
     
-    private func clearOutline() {
-        
-        print("clear outline")
-        
-        DispatchQueue.main.async {
-            self.testingTextView.string = ""
-        }
-    }
+//    private func clearOutline() {
+//
+//        print("clear outline")
+//
+//        DispatchQueue.main.async {
+//            self.testingTextView.string = ""
+//        }
+//    }
     
     // MARK: - OutlineModelDelegate
     func didUpdate(textGroups: [TextGroup]?) {
         
         updateOutline(textGroups: textGroups)
+    }
+    
+    // MARK: - NSOutlineViewDataSource
+    func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
+        
+        // TODO: clean up
+        if item == nil {
+            // root item
+            guard parentTextGroup.textGroups.indices.contains(index) else {
+                
+                return TextGroup(title: "Error")
+            }
+            
+            return parentTextGroup.textGroups[index]
+        }
+        else {
+            
+            guard let textGroup = item as? TextGroup, textGroup.textGroups.indices.contains(index) else {
+                
+                return TextGroup(title: "Error")
+            }
+            
+            return textGroup.textGroups[index]
+        }
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, isItemExpandable item: Any) -> Bool {
+        
+        guard let textGroup = item as? TextGroup else {
+            return false
+        }
+        
+        return !textGroup.textGroups.isEmpty
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
+        
+        if item == nil {
+            // root item
+            return parentTextGroup.textGroups.count
+        }
+        
+        guard let textGroup = item as? TextGroup else {
+            return 0
+        }
+        
+        return textGroup.textGroups.count
+    }
+    
+    // MARK: - NSOutlineViewDelegate
+    func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
+
+        guard let textGroup = item as? TextGroup, let view = outlineView.makeView(withIdentifier: columnReuseIdentifier, owner: self) as? NSTableCellView else {
+            
+            return nil
+        }
+        
+        view.textField?.stringValue = textGroup.title
+        
+        return view
     }
 }
---
+++ Frameworks/TextEditor/TextEditor/Outline/View/OutlineEntry.swift
@@ -0,0 +1,14 @@
+//
+//  OutlineEntry.swift
+//  TextEditor
+//
+//  Created by charles johnston on 11/1/18.
+//  Copyright © 2018 Zin Studio. All rights reserved.
+//
+
+import Foundation
+
+class OutlineEntry: NSTableCellView {
+    
+    @IBOutlet weak var label: NSTextField!
+}
